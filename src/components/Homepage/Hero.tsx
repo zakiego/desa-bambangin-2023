704cbf4bed3c73fb8f3eac46fe92a67c66340395
zakiego <belummikir@gmail.com>
2023-07-04 11:59:51 +0800

    feat: remove announcing tag

--- src/components/Homepage/Hero.tsx
+++ src/components/Homepage/Hero.tsx
@@ -33,13 +33,13 @@ export const Hero: FC<HeroProps> = ({ title, subtitle }) => {
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
+            {/* <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
               Announcing our next round of funding.{" "}
               <a href="#" className="font-semibold text-white">
                 <span className="absolute inset-0" aria-hidden="true" />
                 Read more <span aria-hidden="true">&rarr;</span>
               </a>
-            </div>
+            </div> */}
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
