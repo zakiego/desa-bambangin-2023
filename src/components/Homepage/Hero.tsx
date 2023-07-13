0edefa0fabd8ea5325c8f55d49f22abb0bf14f64
zakiego <belummikir@gmail.com>
2023-07-13 19:19:16 +0800

    feat: add kkn button to home mobile

--- src/components/Homepage/Hero.tsx
+++ src/components/Homepage/Hero.tsx
@@ -33,7 +33,7 @@ export const Hero: FC<HeroProps> = ({ title, subtitle }) => {
           />
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+          <div className="flex mb-8 justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
               KKN Sosiologi 2023 {""}
               <Link href="/kkn" className="font-semibold text-white">
