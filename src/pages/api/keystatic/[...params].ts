73850c61f909cfbeb472ad31097fcca7607c3c36
zakiego <belummikir@gmail.com>
2023-07-13 22:24:08 +0800

    feat: increase bodyParser size limit when dev mode

--- src/pages/api/keystatic/[...params].ts
+++ src/pages/api/keystatic/[...params].ts
@@ -2,4 +2,16 @@ import { makeAPIRouteHandler } from "@keystatic/next/api";
 
 import keystaticConfig from "~/keystatic.config";
 
+export const config =
+  process.env.NODE_ENV === "development"
+    ? {
+        api: {
+          bodyParser: {
+            sizeLimit: "20mb",
+          },
+          responseLimit: "20mb",
+        },
+      }
+    : {};
+
 export default makeAPIRouteHandler({ config: keystaticConfig });
