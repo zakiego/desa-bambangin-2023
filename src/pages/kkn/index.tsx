8bed2a644b9d2c549f42bd00029d6ecdf3fa6a7d
zakiego <belummikir@gmail.com>
2023-07-24 12:51:22 +0800

    feat: remove unused code

--- src/pages/kkn/index.tsx
+++ src/pages/kkn/index.tsx
@@ -8,26 +8,6 @@ import { ContainerContent } from "~/src/components/Layout";
 import { Footer, Navbar } from "~/src/components/UI";
 import { trpcSSR, type TRPCTypeOutput } from "~/src/server/api/root";
 
-const blogPosts = [
-  {
-    id: 1,
-    title: "Vel expedita assumenda placeat aut nisi optio voluptates quas",
-    href: "#",
-    description:
-      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-    imageUrl:
-      "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80",
-    date: "Mar 16, 2020",
-    datetime: "2020-03-16",
-    author: {
-      name: "Michael Foster",
-      imageUrl:
-        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-  },
-  // More posts...
-];
-
 export const getStaticProps = async () => {
   const page = await trpcSSR.keystatic.kkn.page();
   const articles = await trpcSSR.keystatic.kkn.articles.pagination({
